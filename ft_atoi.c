fd39d27e7fd82732bf6e45ad72408e94a6e3e422
Carfern <carfern2@student.42madrid.com>
2024-10-17 16:49:32 +0000

    first upload of basic an additional funtions

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carfern2 <carfern2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 11:30:58 by carfern2          #+#    #+#             */
+/*   Updated: 2024/10/04 08:58:15 by carfern2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * result);
+}
